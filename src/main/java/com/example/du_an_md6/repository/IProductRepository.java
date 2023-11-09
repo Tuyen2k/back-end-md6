95228a09296b721859ddf1f906e7699e686560a2
LongThiPhungOanh <ying49955@gmail.com>
2023-11-09 15:00:23 +0700

    pull dev to local

--- src/main/java/com/example/du_an_md6/repository/IProductRepository.java
+++ src/main/java/com/example/du_an_md6/repository/IProductRepository.java
@@ -1,5 +1,4 @@
 package com.example.du_an_md6.repository;
-
 import com.example.du_an_md6.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
