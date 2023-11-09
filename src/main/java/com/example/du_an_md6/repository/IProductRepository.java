20273af7c7af2b38ae996fff1906863f760b3cea
danhthai3 <ndthai2001@gmail.com>
2023-11-09 14:04:47 +0700

    backend tim kiem product theo id merchant

--- src/main/java/com/example/du_an_md6/repository/IProductRepository.java
+++ src/main/java/com/example/du_an_md6/repository/IProductRepository.java
@@ -3,6 +3,7 @@ package com.example.du_an_md6.repository;
 import com.example.du_an_md6.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,4 +14,9 @@ public interface IProductRepository  extends JpaRepository<Product,Long> {
     List<Product> findProductMerchant(Long id_merchant);
     @Query(value = "SELECT p.* FROM product AS p JOIN merchant AS m ON p.id_merchant = m.id_merchant WHERE m.id_merchant = ? AND p.name LIKE %?% AND p.is_delete = false", nativeQuery = true)
     List<Product> findAllByMerchantAndNameProduct(Long id_merchant, String name);
+    @Query(nativeQuery = true,value = "select p.* from product as p\n" +
+            "join product_category pc on p.id_product = pc.id_product\n" +
+            "where p.name like ?  and pc.id_category = ? \n" +
+            "group by p.id_product ")
+    List<Product> filterProduct(@Param("name") String name, @Param("category") Long id_category );
 }
