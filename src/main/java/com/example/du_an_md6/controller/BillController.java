83459f0dd52cf9008f20f6271af03aad5b4aa9aa
Tuyen2k <tuyen4835@gmail.com>
2023-11-15 17:32:05 +0700

    add billController

---
+++ src/main/java/com/example/du_an_md6/controller/BillController.java
@@ -0,0 +1,14 @@
+package com.example.du_an_md6.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/api/bill")
+public class BillController {
+
+
+
+}
