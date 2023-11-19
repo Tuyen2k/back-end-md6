57b361338a170bdc1322fb5df01ae4d8ba7dfcdd
danghoan95 <hoanvd2210@gmail.com>
2023-11-19 19:53:15 +0700

    check trung lap email , phone name

--- src/main/java/com/example/du_an_md6/service/IAccountService.java
+++ src/main/java/com/example/du_an_md6/service/IAccountService.java
@@ -2,10 +2,14 @@ package com.example.du_an_md6.service;
 
 import com.example.du_an_md6.model.Account;
 import com.example.du_an_md6.model.dto.AccountDTO;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface IAccountService extends IGenerateService<Account> {
     void delete(Long id);
     List<AccountDTO> findAllDTO();
+    Account findByPhone(String phone);
+    Account findByEmail(String email);
+
 }
