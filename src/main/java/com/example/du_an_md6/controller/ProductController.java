3cf168aa5df3ccfc467a2313d09f46252c1b0661
LongThiPhungOanh <ying49955@gmail.com>
2023-11-27 10:14:18 +0700

    fix list product in detail merchant

--- src/main/java/com/example/du_an_md6/controller/ProductController.java
+++ src/main/java/com/example/du_an_md6/controller/ProductController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -19,6 +21,20 @@ public class ProductController {
     @GetMapping("{id_merchant}")
     public ResponseEntity<List<Product>> findProductMerchant (@PathVariable Long id_merchant){
         return new ResponseEntity<>(productService.findProductMerchant(id_merchant), HttpStatus.OK);
+    }
+
+    @GetMapping("/purchase/{id_merchant}")
+    public ResponseEntity<List<Product>> findProductPurchaseMerchant (@PathVariable Long id_merchant){
+        List<Product> p = productService.findProductMerchant(id_merchant);
+        List<Product> productList = new ArrayList<>(p);
+        for (int i = 0; i < productList.size(); i++) {
+            for (int j = i + 1; j < productList.size(); j++) {
+                if (productList.get(i).getPurchase() < productList.get(j).getPurchase()) {
+                    Collections.swap(productList, i, j);
+                }
+            }
+        }
+        return new ResponseEntity<>(productList, HttpStatus.OK);
     }
     @GetMapping("/merchant/{id_product}")
     public ResponseEntity<ProductDTO> findById (@PathVariable Long id_product){
